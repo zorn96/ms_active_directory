8cec9e53d253febdcd9b93e7e0c7d3c5fadec34f
Azaria Zornberg <a.zornberg96@gmail.com>
2021-09-29 23:36:43 -0700

    Fixed exceptions.

--- ms_active_directory/exceptions.py
+++ ms_active_directory/exceptions.py
@@ -67,76 +67,101 @@ class DuplicateNameException(MsActiveDirectoryException):
         super().__init__(exception_str)
 
 
-class InformationUnavailableException(MsActiveDirectoryException):
-    def __init__(self, exception_str):
-        super().__init__(exception_str)
-
-
 class InvalidComputerParameterException(MsActiveDirectoryException):
+    """ An exception raised when functions are called on a ManagedADComputer object with invalid
+    parameters or that rely on unpopulated attributes.
+    """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class InvalidDomainParameterException(MsActiveDirectoryException):
+    """ An exception raised when invalid parameters are used for creating a domain object or
+    establishing a connection with a domain.
+    """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class InvalidLdapParameterException(MsActiveDirectoryException):
+    """ An exception raised when a parameter specified is not of a proper type or format to
+    convert to an LDAP attribute as needed for a function.
+    """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class KeytabEncodingException(MsActiveDirectoryException):
+    """ An exception raised when a keytab is read in from a file but the encoding is invalid """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class LdapResponseDecodeException(MsActiveDirectoryException):
+    """ An exception raised when an LDAP response cannot be parsed properly """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class MembershipModificationException(MsActiveDirectoryException):
+    """ An exception raised when an error is encountered modifying group memberships, and rollback
+    of the incomplete changes was successful.
+    """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class MembershipModificationRollbackException(MsActiveDirectoryException):
+    """ An exception raised when an error is encountered modifying group memberships, but rollback
+    of the incomplete changes was unsuccessful.
+    """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class ObjectCreationException(MsActiveDirectoryException):
+    """ An exception raised when an error is encountered creating an object """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class ObjectNotFoundException(MsActiveDirectoryException):
+    """ An exception raised when an object cannot be found when performing validation that an object
+    exists as part of a function.
+    """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class PermissionDeniedException(MsActiveDirectoryException):
+    """ An exception raised when permission errors occur operating within AD """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class SecurityDescriptorDecodeException(MsActiveDirectoryException):
+    """ An exception raised when errors occur decoding a security descriptor """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class SecurityDescriptorEncodeException(MsActiveDirectoryException):
+    """ An exception raised when errors occur encoding a security descriptor """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class SessionTransferException(MsActiveDirectoryException):
+    """ An exception raised when errors occur transferring an authentication session from
+    one domain to another
+    """
     def __init__(self, exception_str):
         super().__init__(exception_str)
 
 
 class TrustedDomainConversionException(MsActiveDirectoryException):
+    """ An exception raised when trying to convert a trusted domain that has a non-AD type
+    to an ADDomain
+    """
     def __init__(self, exception_str):
         super().__init__(exception_str)
